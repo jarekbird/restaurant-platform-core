6cf1cae35fe8e9eb1e59e5a8c61669553190572c
jarekbird <jarekbird@gmail.com>
2025-11-27 14:30:42 -0700

    remove: Order Now button from center bottom of page
    - Removed CallToActionBar component from preview page
    - Cart button in header is now the primary way to access cart
    - Removed unused CallToActionBar import

--- app/preview/[slug]/page.tsx
+++ app/preview/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { RestaurantLayout } from '@/components/layout/RestaurantLayout';
 import { HeroSection } from '@/components/restaurant/HeroSection';
 import { MenuSectionList } from '@/components/menu/MenuSectionList';
 import { HoursAndLocation } from '@/components/restaurant/HoursAndLocation';
-import { CallToActionBar } from '@/components/restaurant/CallToActionBar';
 import { CartProvider } from '@/components/order/CartProvider';
 import { ChatAssistantWrapper } from '@/components/chat/ChatAssistantWrapper';
 import { ToastProvider } from '@/components/ui/ToastProvider';
@@ -36,14 +35,6 @@ export default async function PreviewPage({ params }: PreviewPageProps) {
           <HeroSection config={config} />
           <MenuSectionList menu={menu} />
           <HoursAndLocation config={config} />
-          {config.orderOnlineEnabled && (
-            <CallToActionBar
-              primaryAction={{
-                label: 'Order Now',
-                href: '#order',
-              }}
-            />
-          )}
         </RestaurantLayout>
         {config.orderOnlineEnabled && <ChatAssistantWrapper menu={menu} />}
       </CartProvider>
